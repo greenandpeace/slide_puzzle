378436966e78748d0a11a95f3953db6197ac7707
坂優志 <banyuji@sakayuushinoMacBook-Pro.local>
2021-01-28 16:16:13 +0900

    san_groupを作りそれにsan_dを登録

--- main.js
+++ main.js
@@ -12,16 +12,19 @@ function gameStart(){
     //==========
 
     scene.backgroundColor = "lightgray"
+    const san_group = new Group();
+    scene.addChild(san_group);
 
     const san = new ExSprite(633,421);
     san.image = core.assets["images/210125_san_01.jpg"];
     //scene.addChild(san);
     for(let i = 1;i<=3;i++){
-
-      let san_d = new ExSprite(633/3*i,421);
-      san_d.image = san.image;
-      scene.addChild(san_d);
-        console.log(i,san_d);
+          for(let i2 = 1;i2<=2;i2++){
+              let san_d = new ExSprite(633/3*i,421/2*i2);
+              san_d.image = san.image;
+              san_group.addChild(san_d);
+              console.log(san_group.childNodes);
+    　}
     }
 
     //==========
