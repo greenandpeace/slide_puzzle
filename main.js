5637ebe3634d73c2f7438a63f44bb6e60cb63e58
坂優志 <banyuji@sakayuushinoMacBook-Pro.local>
2021-01-28 16:07:08 +0900

    とりあえず三分割したものを右から三つだす

--- main.js
+++ main.js
@@ -13,9 +13,16 @@ function gameStart(){
 
     scene.backgroundColor = "lightgray"
 
-    const san = new ExSprite(633/3,421/2);
+    const san = new ExSprite(633,421);
     san.image = core.assets["images/210125_san_01.jpg"];
-    scene.addChild(san);
+    //scene.addChild(san);
+    for(let i = 1;i<=3;i++){
+
+      let san_d = new ExSprite(633/3*i,421);
+      san_d.image = san.image;
+      scene.addChild(san_d);
+        console.log(i,san_d);
+    }
 
     //==========
     // ここまで
